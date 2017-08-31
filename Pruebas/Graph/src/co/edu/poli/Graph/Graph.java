7b23fb28236a2652343f1d724bee2492eab7ac64
jacastrorug <juliancastroruge@hotmail.com>
2017-08-31 12:04:07 -0500

    Matriz de adyacencia
    Se crea la interfaz de matriz de adyacencia y la implementación en una
    matriz que recibe un grafo dirijido.

--- Pruebas/Graph/src/co/edu/poli/Graph/Graph.java
+++ Pruebas/Graph/src/co/edu/poli/Graph/Graph.java
@@ -1,5 +1,5 @@
 package co.edu.poli.Graph;
 
 public interface Graph {
-
+	
 }
---
+++ Pruebas/Graph/src/co/poli/edu/MatrixAdj/MatrixDirected.java
@@ -0,0 +1,12 @@
+package co.poli.edu.MatrixAdj;
+
+import co.edu.poli.Graph.GraphDirect;
+
+public class MatrixDirected {
+	double matAdj[][];
+	GraphDirect graph;
+	public MatrixDirected() {
+		
+		
+	}
+}
