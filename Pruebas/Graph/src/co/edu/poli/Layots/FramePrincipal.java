35b70b2598d033295a11360bb024cae569daf047
dayepesb <lion17-07@hotmail.com>
2017-10-05 00:36:52 -0500

    Correciones en frame Principal
    Se corrigio el bus de la lista de los algoritmos

--- Pruebas/Graph/src/co/edu/poli/Layots/FramePrincipal.java
+++ Pruebas/Graph/src/co/edu/poli/Layots/FramePrincipal.java
@@ -1,6 +1,7 @@
 package co.edu.poli.Layots;
 
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
@@ -12,22 +13,26 @@ import java.awt.event.MouseListener;
 import java.io.IOException;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 
 import co.edu.poli.Lists.ListProperties;;
 
-public class FramePrincipal extends JFrame implements MouseListener, ActionListener,ComponentListener {
+public class FramePrincipal extends JFrame implements MouseListener, ActionListener, ComponentListener {
 	private static final long serialVersionUID = -8099070856715541164L;
+	private final Border border = LineBorder.createGrayLineBorder();
 	private JMenuBar mb;
 	private JMenu file;
 	private JMenuItem close;
 	private Dimension dim;
-
+	private JLabel labelAlgorithms;
 	private JList<String> lista;
 	private JPanel panel;
 	private JScrollPane barraDesplazamiento;
@@ -38,8 +43,8 @@ public class FramePrincipal extends JFrame implements MouseListener, ActionListe
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		dim = Toolkit.getDefaultToolkit().getScreenSize();
 		setSize(dim.width * 7 / 10, dim.height * 8 / 10);
-		setMinimumSize(new Dimension(956,614));
-		System.out.println(dim.width * 7 / 10+" "+ dim.height * 8 / 10);
+		setMinimumSize(new Dimension(956, 614));
+		System.out.println(dim.width * 7 / 10 + " " + dim.height * 8 / 10);
 		setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2);
 		setLayout(new GridLayout());
 		this.addComponentListener(this);
@@ -57,11 +62,17 @@ public class FramePrincipal extends JFrame implements MouseListener, ActionListe
 		panel = new JPanel();
 		panel.setLayout(null);
 
+		// Se agrega una etiqueta de label
+		labelAlgorithms = new JLabel("Algorithms");
+		labelAlgorithms.setBorder(border);
+		panel.add(labelAlgorithms);
+
 		// creación de los elememtos que componen la lista
 		// creación del objeto lista
 		lista = new ListProperties();
 		lista.addMouseListener(this);
-		
+
+
 		// aquí se crea el objeto, es decir la barra de desplazamiento
 		barraDesplazamiento = new JScrollPane(lista);
 		// Agrega la barra de desplazamiento al panel
@@ -71,45 +82,66 @@ public class FramePrincipal extends JFrame implements MouseListener, ActionListe
 		setVisible(true);
 	}
 
+	private void resizedComponents() {
+		labelAlgorithms.setText("Algorithms");
+		labelAlgorithms.setFont(new Font("TIMES NEW ROMAN", 0, 15));
+		int anchoLabelAlgorithms = (int) (this.getWidth() * (2) / 10);
+		int largoLabelAlgorithms = (int) (this.getHeight() *0.7/ 20);
+		int xLabelAlgorithms = this.getWidth() - anchoLabelAlgorithms - 15;
+		int yLabelAlgorithms = 0;
+		labelAlgorithms.setBounds(xLabelAlgorithms,yLabelAlgorithms,anchoLabelAlgorithms,largoLabelAlgorithms);
+
+		lista.setFont(new Font("TIMES NEW ROMAN", 0, 15));
+		int anchoList = (int) (this.getWidth() * (2) / 10);
+		int largoList = this.getHeight() * 8 / 30;
+		int xList = this.getWidth() - anchoList - 15;
+		int yList = largoLabelAlgorithms;
+		barraDesplazamiento.setBounds(xList, yList, anchoList, largoList);
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource().equals(close)) {
 			dispose();
 		}
 	}
+
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		if(e.getSource().equals(lista)) {
+		if (e.getSource().equals(lista)) {
 			System.out.println(lista.getSelectedValue());
 		}
 	}
+
 	@Override
 	public void mouseEntered(MouseEvent e) {
 	}
+
 	@Override
 	public void mouseExited(MouseEvent e) {
 	}
+
 	@Override
 	public void mousePressed(MouseEvent e) {
 	}
+
 	@Override
 	public void mouseReleased(MouseEvent e) {
 	}
+
 	@Override
 	public void componentHidden(ComponentEvent e) {
 	}
+
 	@Override
 	public void componentMoved(ComponentEvent e) {
 	}
+
 	@Override
 	public void componentResized(ComponentEvent e) {
-		int anchoList = (int) (this.getWidth()*(1.5)/10);
-		int largoList = this.getHeight()* 8 / 20;
-		int x = this.getWidth()-anchoList-15;
-		int y = 0;
-		barraDesplazamiento.setBounds(x, y,anchoList,largoList);
-	
+		resizedComponents();
 	}
+
 	@Override
 	public void componentShown(ComponentEvent e) {
 	}
