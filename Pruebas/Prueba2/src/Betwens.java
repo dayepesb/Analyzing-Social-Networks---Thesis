18838838f8e3cfdec4e9f4bf9fbddfc2a6fa3491
Julian Castro <juliancastroruge@hotmail.com>
2017-10-13 11:48:38 -0500

    Revert "Clloseness y DegreeCentrality"
    This reverts commit dcd5cbb652fa2f40bf00f987fe73febbb534121d.

--- Pruebas/Prueba2/src/Betwens.java
+++ Pruebas/Prueba2/src/Betwens.java
@@ -21,40 +21,22 @@ public class Betwens {
 		Node E = graph.addNode("E");
 		Node C = graph.addNode("C");
 		Node D = graph.addNode("D");
-		Node F = graph.addNode("F");
-		
-		//A.addAttribute("ui.style", "");
-		
 
 		graph.addEdge("AB", "A", "B");
-		//graph.addEdge("BE", "B", "E");
+		graph.addEdge("BE", "B", "E");
 		graph.addEdge("BC", "B", "C");
-		//graph.addEdge("ED", "E", "D");
+		graph.addEdge("ED", "E", "D");
 		graph.addEdge("CD", "C", "D");
 		graph.addEdge("AE", "A", "E");
-		//graph.addEdge("EC", "E", "C");
-		//graph.addEdge("BD", "B", "D");
-		graph.addEdge("EF", "E", "F");
 
 		BetweennessCentrality bcb = new BetweennessCentrality();
 		bcb.setWeightAttributeName("weight");
-		bcb.setWeight(A, B, 100);
-		bcb.setWeight(B, E, 1);
-		bcb.setWeight(B, C,100);
-		bcb.setWeight(E, D, 1);
-		bcb.setWeight(C, D, 1);
-		bcb.setWeight(A, E, 1);
-		bcb.setWeight(E, C, 1);
-		bcb.setWeight(B, D, 1);
-		
-		
 		bcb.setWeight(A, B, 1);
 		bcb.setWeight(B, E, 6);
 		bcb.setWeight(B, C, 5);
 		bcb.setWeight(E, D, 2);
 		bcb.setWeight(C, D, 3);
 		bcb.setWeight(A, E, 4);
-		
 		bcb.init(graph);
 		bcb.compute();
 
@@ -66,10 +48,7 @@ public class Betwens {
 		
 		for (Node n: graph.getNodeSet()) {
 			n.addAttribute("label", n.getId());
-			n.addAttribute("ui.style", "size: "+((((double)n.getAttribute("Cb"))*2.5)+10)+"px;");
 		}
-		
-		
 		
 		graph.display();
 	}
