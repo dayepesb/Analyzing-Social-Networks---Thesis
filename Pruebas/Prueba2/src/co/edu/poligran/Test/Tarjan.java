9364c90d40640a39730c4a09f0989308fd8d5c23
dayepesb <lion17-07@hotmail.com>
2017-10-17 16:49:09 -0500

    Correcciones

--- Pruebas/Prueba2/src/co/edu/poligran/Test/Tarjan.java
+++ Pruebas/Prueba2/src/co/edu/poligran/Test/Tarjan.java
@@ -1,5 +1,8 @@
 package co.edu.poligran.Test;
+import java.util.Arrays;
+
 import org.graphstream.algorithm.TarjanStronglyConnectedComponents;
+import org.graphstream.algorithm.Toolkit;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.MultiGraph;
@@ -19,6 +22,10 @@ public class Tarjan {
 					edges.substring(2 * i + 1, 2 * i + 2), true);
 		}
 
+		System.out.println(Toolkit.density(g));
+		System.out.println(Toolkit.diameter(g));
+		System.out.println(Arrays.toString(Toolkit.clusteringCoefficients(g)));
+
 		g.display();
 
 		TarjanStronglyConnectedComponents tscc = new TarjanStronglyConnectedComponents();
@@ -27,7 +34,7 @@ public class Tarjan {
 
 		for (Node n : g.getEachNode()){
 			String k=tscc.getSCCIndexAttribute()+"";
-			n.addAttribute("label", n.getAttribute(k));
+			n.addAttribute("label", n.getAttribute(k).toString());
 
 		}
 	}
