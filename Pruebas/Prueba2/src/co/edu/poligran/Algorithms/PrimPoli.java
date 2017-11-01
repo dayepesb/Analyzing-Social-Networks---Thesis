97a7ebcc68815390ba29955d28df0a41df80039a
Julian Castro <juliancastroruge@hotmail.com>
2017-10-31 21:53:24 -0500

    Correcciones 2

---
+++ Pruebas/Prueba2/src/co/edu/poligran/Algorithms/PrimPoli.java
@@ -0,0 +1,101 @@
+package co.edu.poligran.Algorithms;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+
+import org.graphstream.algorithm.generator.DorogovtsevMendesGenerator;
+import org.graphstream.algorithm.generator.Generator;
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.MultiGraph;
+
+public class PrimPoli {
+
+	public static void main(String[] args) {
+		Graph temp = new MultiGraph("as");
+		Generator gen = new DorogovtsevMendesGenerator();
+		gen.addSink(temp);
+		gen.begin();
+		for (int i = 0; i < 100; i++) {
+			gen.nextEvents();
+		}
+		gen.end();
+		temp.display();
+		PrimPoli prim = new PrimPoli(temp);
+		prim.compute();
+
+	}
+
+	private Graph graph;
+
+	public PrimPoli(Graph graph) {
+		this.graph = graph;
+		for (Edge e : this.graph.getEachEdge()) {
+			if (!e.hasAttribute("-attribute-width"))
+				e.setAttribute("-attribute-width", 1.0);
+		}
+	}
+
+	public void compute() {
+		PriorityQueue<Edge> q = new PriorityQueue<Edge>(graph.getEdgeCount(), new Comparator<Edge>() {
+			@Override
+			public int compare(Edge a, Edge b) {
+				double widthA = (double) a.getAttribute("-attribute-width");
+				double widthB = (double) b.getAttribute("-attribute-width");
+				return Double.compare(widthA, widthB);
+			}
+		});
+		ArrayList<Node> nodes = new ArrayList<Node>();
+		HashSet<Integer> nodesHash = new HashSet<Integer>();
+		HashSet<Integer> edgesHash = new HashSet<Integer>();
+		Node nodeRandom = null;
+		for (Node n : graph) {
+			nodeRandom = n;
+			break;
+		}
+		q.addAll(nodeRandom.getEdgeSet());
+		nodes.add(nodeRandom);
+		nodesHash.add(nodeRandom.getIndex());
+		while (!q.isEmpty()) {
+			Edge p = q.poll();
+			if (p != null) {
+				Node A = p.getNode0();
+				Node B = p.getNode1();
+				if (!nodesHash.contains(A.getIndex())) {
+					nodes.add(A);
+					q.addAll(A.getEdgeSet());
+					nodesHash.add(A.getIndex());
+					edgesHash.add(p.getIndex());
+				} else if (!nodesHash.contains(B.getIndex())) {
+					nodes.add(B);
+					q.addAll(B.getEdgeSet());
+					nodesHash.add(B.getIndex());
+					edgesHash.add(p.getIndex());
+				}
+			}
+		}
+
+		graph.setAttribute("ui.style", "background-color: black;");
+
+		for (Edge e : graph.getEdgeSet()) {
+			if (!edgesHash.contains(e.getIndex())) {
+				e.setAttribute("ui.style", "fill-color: #000;");
+			} else
+				e.setAttribute("ui.style", "fill-color: #FFF;");
+
+		}
+
+		for (Node n : graph) {
+			if (!nodesHash.contains(n.getIndex())) {
+				n.setAttribute("ui.style", "fill-color: #000;");
+			} else
+				n.setAttribute("ui.style", "fill-color: #FFF;");
+
+		}
+
+	}
+
+}
